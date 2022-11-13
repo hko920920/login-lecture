b55541243d39a3dc1d03d37093dc53c85f9f26f3
Ko Hankyeong <cody@Kos-MacBook-Pro.local>
2022-11-13 15:25:47 +0900

    프런트의 요청데이터 파싱

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const home = require("./src/routes/home");
 
@@ -8,6 +10,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+//URL을 통해 전달되는 데이터에 한글, 공백등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/", home);
 
