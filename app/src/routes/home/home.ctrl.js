5075ea1c4e0bc7d98fe2df84daf7b23eb77c6960
Ko Hankyeong <cody@Kos-MacBook-Pro.local>
2022-11-13 15:58:58 +0900

    로그인 인증 기능 만들기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const { is } = require("type-is");
+
+
+
 const output = {
     hello: (req, res) => {
         res.render("home/index");
@@ -10,11 +14,30 @@ const output = {
     }
 };
 
+const users ={
+    id: ["woorimIT", "나개발", "김팀장"],
+    psword: ["1234", "1234", "123456"]  
+  };
 const process ={
     login: (req,res) =>{
-        console.log(req.body);
-    }
-}
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "failed to login.", 
+        })
+    },
+};
 
 module.exports = {
     output,
