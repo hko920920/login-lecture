63f0f6129b077a136f2fe9423444d03a0f8271ca
Ko Hankyeong <cody@Kos-MacBook-Pro.local>
2022-11-14 21:14:29 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,8 +1,7 @@
 "use strict";
 
 const { is } = require("type-is");
-
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     hello: (req, res) => {
@@ -17,23 +16,10 @@ const output = {
 
 const process ={
     login: (req,res) =>{
-        const id = req.body.id,
-            psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id","psword");
-
-        const response ={};
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "failed to login.";
-        return res.json(response);
-    },
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
+    }
 };
 
 module.exports = {
